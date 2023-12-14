73559729b0a59a89c1f8be5df4dfd724c9f5fd0d
Welley <monteirowelley7@gmail.com>
2023-12-14 08:43:25 -0300

    fundamentos

---
+++ react-fundamentais/src/App.jsx
@@ -0,0 +1,12 @@
+function sum(a,b) {
+  return a + b
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <h2>4 + 4 is: {sum(4,4)}</h2>
+    </div>
+  )
+}
