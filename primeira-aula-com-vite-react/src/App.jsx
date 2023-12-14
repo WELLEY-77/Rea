e363973448299f72e57819023cb1ba0b4d5c90f8
Welley <monteirowelley7@gmail.com>
2023-12-14 08:28:12 -0300

    React desafios

---
+++ primeira-aula-com-vite-react/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Meu primeiro projeto Vite com React</h1>
+      <h2>Proximos passos</h2>
+      <ul>
+        <li>Estudar</li>
+        <li>Estudar React</li>
+        <li>Estudar JS</li>
+      </ul>
+    </div>
+  )
+}
+
+
